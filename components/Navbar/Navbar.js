16e6e007cffd114de17ccd62351bf4afd2bf5be3
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-03-31 16:35:14 +0300

    Add Navbar component

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-primary navbar-dark" id="nav-bar-container">
+            <Link href="/"><a className="navbar-brand">Grades System</a></Link>
+            {/* <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" type="button" id="showAll" href="#">Show All</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link 2</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link 3</a>
+                </li>
+            </ul> */}
+        </nav>
+    );
+}
+
+export default Navbar;
