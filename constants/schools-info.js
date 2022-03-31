fe65a421621d580a35c8c0aa9ffe4547cc2e1985
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-03-31 18:55:05 +0300

    Add schools info constants

---
+++ constants/schools-info.js
@@ -0,0 +1,79 @@
+module.exports = {
+    schools: [
+        "SCHOOL OF CIVIL ENGINEERING",
+        "SCHOOL OF MECHANICAL ENGINEERING",
+        "SCHOOL OF ELECTRICAL & COMPUTER ENGINEERING",
+        "SCHOOL OF ARCHITECTURE",
+        "SCHOOL OF CHEMICAL ENGINEERING",
+        "SCHOOL OF RURAL, SURVEYING AND GEOINFORMATICS ENGINEERING",
+        "SCHOOL OF MINING & METALLURGICAL ENGINEERING",
+        "SCHOOL OF NAVAL ARCHITECTURE & MARINE ENGINEERING",
+        "SCHOOL OF APPLIED MATHEMATICAL & PHYSICAL SCIENCES",
+    ],
+
+    courses: {
+        "SCHOOL OF CIVIL ENGINEERING": [
+            { id: 1001, name: "Numerical Analysis" },
+            { id: 1002, name: "Irrigation Engineering" },
+            { id: 1003, name: "Airport Planning and Management" },
+            { id: 1005, name: "Topics on Architecture & Architectural Synthesis" },
+            { id: 1007, name: "Geology for Engineers" },
+        ],
+        "SCHOOL OF MECHANICAL ENGINEERING": [
+            { id: 2001, name: "Nuclear Power Reactor Set-up and Operation" },
+            { id: 2007, name: "Modelling and Automatic Control of Systems" },
+            { id: 2008, name: "Mathematics A1" },
+            { id: 2009, name: "Industrial Refrigeration Systems" },
+            { id: 2010, name: "Mechanics C" },
+        ],
+        "SCHOOL OF ELECTRICAL & COMPUTER ENGINEERING": [
+            { id: 3047, name: "High Voltage Measurements and Applications" },
+            { id: 3048, name: "Philosophy" },
+            { id: 3049, name: "Physics I" },
+            { id: 3051, name: "Nuclear Physics and Elementary Particles" },
+            { id: 3057, name: "Microwaves" },
+        ],
+        "SCHOOL OF ARCHITECTURE": [
+            { id: 4204, name: "Structures 1: Introduction to Structural Analysis" },
+            { id: 4205, name: "Mathematics" },
+            { id: 4207, name: "History and Theory 2nd Semester: From the Roman to the Byzantine Period" },
+            { id: 4208, name: "Building Technology 1: Small-Scale Buildings" },
+            { id: 4211, name: "Systematic Analysis of Vernacular Buildings and Settlements" },
+        ],
+        "SCHOOL OF CHEMICAL ENGINEERING": [
+            { id: 5273, name: "Statistics and Experimental Design" },
+            { id: 5274, name: "Applied Didactics of Natural Sciences and Technology" },
+            { id: 5275, name: "History and Philosophy of Science and Technology" },
+            { id: 5276, name: "Organic Chemistry" },
+            { id: 5277, name: "Quality Management" },
+        ],
+        "SCHOOL OF RURAL, SURVEYING AND GEOINFORMATICS ENGINEERING": [
+            { id: 6032, name: "Cartography I (General Cartography)" },
+            { id: 6033, name: "River Engineering" },
+            { id: 6034, name: "Geophysical Exploration - Gravimetry" },
+            { id: 6042, name: "Probability Theory and Statistics" },
+            { id: 6043, name: "Theory of Errors and Adjustments I" },
+        ],
+        "SCHOOL OF MINING & METALLURGICAL ENGINEERING": [
+            { id: 7136, name: "Modern methods in topographic mapping" },
+            { id: 7143, name: "Chemical Kinetics" },
+            { id: 7145, name: "Elements of reinforced concrete and steel structures" },
+            { id: 7151, name: "Physical metallurgy - The alloys" },
+            { id: 7153, name: "Ceramic Materials" },
+        ],
+        "SCHOOL OF NAVAL ARCHITECTURE & MARINE ENGINEERING": [
+            { id: 8145, name: "Computer Aided Mechanical Drawing" },
+            { id: 8150, name: "Mechanics of Deformable Solids II" },
+            { id: 8156, name: "Applied Functional Analysis (with applications in engineering problems)" },
+            { id: 8157, name: "CAD/CAM Systems for Ship Design and Construction" },
+            { id: 8158, name: "Science and Technology of Welding" },
+        ],
+        "SCHOOL OF APPLIED MATHEMATICAL & PHYSICAL SCIENCES": [
+            { id: 9053, name: "Real Analysis I" },
+            { id: 9054, name: "Numerical Analysis II and Laboratory" },
+            { id: 9056, name: "Introduction to Continuum Mechanics" },
+            { id: 9057, name: "Discrete Mathematics" },
+            { id: 9058, name: "Algebra and Applications" },
+        ],
+    }
+}
