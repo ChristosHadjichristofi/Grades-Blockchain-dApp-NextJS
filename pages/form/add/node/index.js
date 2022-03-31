1e61c2249b4982500f4f30c9814723e041ee6027
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-03-31 18:55:40 +0300

    Create Add participant page

--- pages/form/add/node/index.js
+++ pages/form/add/node/index.js
@@ -1,3 +1,39 @@
+import { schools } from "../../../../constants/schools-info";
+import styles from './NodeForm.module.css';
+
 export default function AddNodeFormPage() {
-    return <div></div>;
+  return (
+    <div>
+      <div className={`container ${styles["container-form"]}`}>
+        <div className="card">
+          <div className="card-body">
+            <h6 className="text-muted card-subtitle mb-2">Complete the following form</h6>
+
+            <form method="POST" action="/api/add/node/permissions">
+              <label htmlFor="wallet">Node Wallet</label>
+              <input className="form-control" type="text" name="wallet" id="wallet"/>
+
+              <label htmlFor="schools">School</label>
+              <select className="form-control" id="schools" name="school">
+                {schools.map((school) => {
+                  return <option value={school}>{school}</option>;
+                })}
+              </select>
+
+              <label htmlFor="master">Master Node</label>
+              <select className="form-control" id="master" name="master">
+                <option value="No">No</option>
+                <option value="Yes">Yes</option>
+              </select>
+
+              <div className={styles["btn-container"]}>
+                <button className={`btn btn-primary ${styles["btn-space"]}`} type="submit">Save</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
