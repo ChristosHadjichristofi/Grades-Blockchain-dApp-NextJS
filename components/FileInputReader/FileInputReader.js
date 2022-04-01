7fab72e49b1f1aabfc42b28f625ffc20dde048ac
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-01 18:19:57 +0300

    Create FileInputReader.js

---
+++ components/FileInputReader/FileInputReader.js
@@ -0,0 +1,28 @@
+const FileImport = ({ setFileContent }) => {
+    let fileReader;
+
+    const handleFileRead = (e) => {
+        const content = fileReader.result;
+        setFileContent(content);
+        // console.log(content)
+        // … do something with the 'content' …
+    };
+
+    const handleFileChosen = (file) => {
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file);
+    };
+
+    return <div className='upload-expense'>
+        <input
+            type='file'
+            id='file'
+            className='input-file'
+            accept='.bau'
+            onChange={e => handleFileChosen(e.target.files[0])}
+        />
+    </div>;
+};
+
+export default FileImport;
