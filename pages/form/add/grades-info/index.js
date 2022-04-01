c60e355d75d18d5102ac9861c3a208e02055225c
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-01 10:55:16 +0300

    Complete grades info page (not functionality)

--- pages/form/add/grades-info/index.js
+++ pages/form/add/grades-info/index.js
@@ -1,75 +1,99 @@
-import { useState } from "react";
+import styles from './GradesInfoForm.module.css';
+import { useState, useContext, useRef } from "react";
 import { schools, courses } from "../../../../constants/schools-info";
+import UserContext from "../../../../store/user-context";
 
 export default function AddGradeDetailsFormPage() {
 
-    const [selectedSchool, setSelectedSchool] = useState("");
+    const userCtx = useContext(UserContext);
+    const [selectedSchool, setSelectedSchool] = useState(userCtx.user.isMaster ? "SCHOOL OF CIVIL ENGINEERING" : userCtx.user.school);
+
+    const schoolInput = useRef();
+    const periodInput = useRef();
+    const courseInput = useRef();
+    const profInput = useRef();
+    const examDateInput = useRef();
+    const participantsInput = useRef();
+    const passedInput = useRef();
+    const gradesAssetInput = useRef();
+    const updateStatusInput = useRef();
+    const notesRef = useRef();
+    const gradesFileRef = useRef();
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+
+    }
+
+    function optionChanged(e) {
+        setSelectedSchool(e.target.value);
+    }
 
     return (
-        <div class="container" id="container-form">
-            <div class="card">
-                <div class="card-body">
-                    <h6 class="text-muted card-subtitle mb-2">Complete the following form</h6>
-                    <form method="POST" action="api/store/form" enctype="multipart/form-data">
-                        <label for="schools">School</label>
-                        {/* { (isMaster) ?  */}
-                            <select class="form-control" id="schools" name="school">
+        <div className={`container ${styles["container-form"]}`}>
+            <div className="card">
+                <div className="card-body">
+                    <h6 className="text-muted card-subtitle mb-2">Complete the following form</h6>
+                    <form encType="multipart/form-data" onSubmit={submitHandler}>
+                        <label htmlFor="schools">School</label>
+                        {(userCtx.user.isMaster) ?
+                            <select className="form-control" id="schools" name="school" onChange={optionChanged}>
                                 {schools.map((school) => {
-                                    return <option value={school}>{school}</option>;
+                                    return <option key={school} value={school}>{school}</option>;
                                 })}
-                            </select> 
-                        {/* :
-                            <select class="form-control" id="schools" name="school" onchange="optionChanged()">
-                                <option value={ user.school }>{ user.school }</option>
                             </select>
-                        } */}
+                            :
+                            <select className="form-control" id="schools" name="school">
+                                <option value={userCtx.user.school}>{userCtx.user.school}</option>
+                            </select>
+                        }
 
-                        <label for="period">Period</label>
-                        <select class="form-control" id="period" name="period">
+                        <label htmlFor="period">Period</label>
+                        <select className="form-control" id="period" name="period">
                             <option value="WINTER">WINTER</option>
                             <option value="SUMMER">SUMMER</option>
                             <option value="AUTUMN">AUTUMN</option>
                         </select>
 
-                        <label for="course">Course</label>
-                        <select class="form-control" name="course" id="courses">
-                                {schools.map((school) => {
-                                    return <option value={school}>{school}</option>;
-                                })}
-                            {/* <option value="<%= course.code %>"><%= course.code %> - <%= course.name %></option> */}
+                        <label htmlFor="course">Course</label>
+                        <select className="form-control" name="course" id="courses">
+                            {courses[selectedSchool].map((course) => {
+                                return <option key={course.id} value={course.id}>{course.name + ' - ' + course.id}</option>;
+                            })}
                         </select>
 
-                        <label for="professor">Professor</label>
-                        <input class="form-control" name="professor" id="professor"/>
+                        <label htmlFor="professor">Professor</label>
+                        <input className="form-control" name="professor" id="professor" />
 
-                        <label for="exam-date">Exam Date</label>
-                        <input class="form-control" type="datetime-local" name="exam_date" id="exam-date"/>
+                        <label htmlFor="exam-date">Exam Date</label>
+                        <input className="form-control" type="datetime-local" name="exam_date" id="exam-date" />
 
-                        <label for="participants-no">Number of Participants</label>
-                        <input class="form-control" type="number" name="participants_no" id="participants-no"/>
+                        <label htmlFor="participants-no">Number of Participants</label>
+                        <input className="form-control" type="number" name="participants_no" id="participants-no" />
 
-                        <label for="pass-no">Number of Participants Passed</label>
-                        <input class="form-control" type="number" name="pass_no" id="pass-no"/>
+                        <label htmlFor="pass-no">Number of Participants Passed</label>
+                        <input className="form-control" type="number" name="pass_no" id="pass-no" />
 
-                        <label for="grades-asset-url">Grades Asset</label>
-                        <input class="form-control" type="url" name="grades_asset_url" id="grades-asset-url"/>
+                        <label htmlFor="grades-asset-url">Grades Asset</label>
+                        <input className="form-control" type="url" name="grades_asset_url" id="grades-asset-url" />
 
-                        <label for="update-status">Update Status</label>
-                        <select class="form-control" id="update_status" name="update_status">
+                        <label htmlFor="update-status">Update Status</label>
+                        <select className="form-control" id="update_status" name="update_status">
                             <option value="INITIAL STATE">INITIAL STATE</option>
                             <option value="CORRECTIVE STATE">CORRECTIVE STATE</option>
                         </select>
 
-                        <label for="notes">Notes</label>
-                        <textarea class="form-control" name="notes" id="notes"></textarea>
+                        <label htmlFor="notes">Notes</label>
+                        <textarea className="form-control" name="notes" id="notes"></textarea>
 
                         <label>Grades File (.bau)</label>
                         <div>
-                            <input type="file" name="grades_file" accept=".bau"/>
+                            <input type="file" name="grades_file" accept=".bau" />
                         </div>
 
-                        <div id="btn-container">
-                            <button class="btn btn-primary" id="storeGrades" type="submit">Save</button>
+                        <div className={styles["btn-container"]}>
+                            <button className={`btn btn-primary ${styles["storeGrades"]}`}>Save</button>
                         </div>
                     </form>
                 </div>
