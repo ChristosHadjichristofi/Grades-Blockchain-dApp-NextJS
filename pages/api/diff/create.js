2b0019009e5a355e45838f9baa23bcca454334a7
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-04 09:40:19 +0300

    Create /api/diff/create route

---
+++ pages/api/diff/create.js
@@ -0,0 +1,47 @@
+const Diff = require('diff');
+const Diff2html = require('diff2html');
+const fs = require('fs');
+
+export default async function handler(req, res) {
+    const { grades_asset_url, grades_asset_content_base64 } = req.body;
+
+    const response = await fetch(grades_asset_url, { mode: 'no-cors' });
+    const data = await response.text();
+    // convert from base64 to ascii
+    let grades_asset_content;
+    let asset_url_content;
+    grades_asset_content = Buffer.from(grades_asset_content_base64, 'base64').toString('ascii');
+    asset_url_content = data;
+
+    // check the content of the grades_asset (which is on the blockchain)
+    // with the content of the url
+    // keep all added and removed parts to construct a file (if anything has changed)
+    let added = [], removed = [];
+    const diff = Diff.diffLines(grades_asset_content, asset_url_content);
+
+    diff.forEach(part => {
+        if (part.added) added.push(part.value);
+        if (part.removed) removed.push(part.value);
+    });
+
+    const diffJson = Diff2html.parse(Diff.createTwoFilesPatch('file_from_blockchain.bau', 'file_from_url.bau', grades_asset_content, asset_url_content));
+    const diffHtml = Diff2html.html(diffJson, {
+        drawFileList: true,
+        fileListToggle: false,
+        fileListStartVisible: false,
+        fileContentToggle: false,
+        matching: 'lines',
+        outputFormat: 'side-by-side',
+        synchronisedScroll: true,
+        highlight: true,
+        renderNothingWhenEmpty: false,
+    });
+
+    if (added.length == 0 && removed.length == 0) return res.status(200).json({ error: false });
+    else {
+        fs.writeFileSync('public/downloads/file_from_blockchain.bau', grades_asset_content);
+        fs.writeFileSync('public/downloads/file_from_url.bau', asset_url_content);
+
+        return res.status(200).json({ error: true, diff: diffHtml });
+    }
+}
