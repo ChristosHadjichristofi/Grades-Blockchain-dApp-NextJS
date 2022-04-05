747b9ade0f63ecfc4904dc746449724ee5fbc9f3
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-05 10:24:58 +0300

    Update Navbar Component

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,18 +1,33 @@
 import Link from 'next/link';
 import { useWeb3React } from "@web3-react/core";
 import styles from "./Navbar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignIn } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     const { active } = useWeb3React();
+    
     return (
         <nav className={`navbar navbar-expand-lg bg-primary navbar-dark ${styles['nav-bar-container']}`}>
-            {active && <Link href="/menu"><a className="navbar-brand">Grades System</a></Link>}
+            <div>
+                <a className="navbar-brand"><img className={`${styles.icon} ${styles['icon-round-background']}`} src="/images/ntua_logo.png" /> ECE NTUA @ Softlab</a>
+            </div>
 
             <ul className="navbar-nav">
+                {!active && 
                 <li className="nav-item">
-                    <a className="nav-link">
-                        <img className={`${styles.icon} ${styles['icon-round-background']}`} src="/images/ntua_logo.png" /> ECE NTUA @ Softlab</a>
+                    <Link href="/">
+                        <a className="nav-link"><FontAwesomeIcon icon={ faSignIn }/>&nbsp;Login</a>
+                    </Link>
+                </li>}
+
+                {active &&
+                <li className="nav-item">
+                    <Link href="/menu">
+                        <a className="nav-link">Menu</a>
+                    </Link>
                 </li>
+                }                
             </ul>
 
         </nav>
