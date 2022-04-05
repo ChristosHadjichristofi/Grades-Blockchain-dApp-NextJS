92c56a8e6d00444065412e56d3d99e8e013fbc7b
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-05 10:24:43 +0300

    Update Footer Component

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,15 +2,16 @@ import { faFileText, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Footer.module.css';
+import Link from "next/link";
 
 const Footer = () => {
     return (
         <footer id="footer" className={styles['footer-basic']}>
             <ul className="list-inline">
                 <li className="list-inline-item">
-                    <a href="/">
-                        <FontAwesomeIcon icon={ faInfoCircle }/>&nbsp;About
-                    </a>
+                    <Link href="/about">
+                        <a><FontAwesomeIcon icon={ faInfoCircle }/>&nbsp;About</a>
+                    </Link>
                 </li>
                 <li className="list-inline-item">
                     <a href="/">
@@ -18,12 +19,12 @@ const Footer = () => {
                     </a>
                 </li>
                 <li className="list-inline-item">
-                    <a href="https://github.com/ChristosHadjichristofi/Grades-Blockchain-App">
+                    <a href="https://github.com/ChristosHadjichristofi/Grades-Blockchain-App-NextJS">
                         <FontAwesomeIcon icon={ faGithub }/>&nbsp;Github
                     </a>
                 </li>
             </ul>
-            <p className={styles.copyright}>Grades System App</p>
+            <p className={styles.copyright}>Grades System dApp</p>
         </footer>
     );
 }
