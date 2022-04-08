f0b9c719b892f3c374203171370af90003fef9b7
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-08 12:00:37 +0300

    Update Card

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -4,12 +4,13 @@ import styles from "./Card.module.css";
 export default function Card(props) {
     return (
         <div className="col-md-4">
-            <div className={`card ${styles["card-container-layout"]}`}>
+            <div className={`card ${styles["card-container-layout"]} ${styles.shadow}`}>
                 <div className={`card-body ${styles["card-center"]}`}>
                     <h5 className="card-title">{props.title}</h5>
+                    <hr/>
                     <p className="card-text">{props.details}</p>
                     <Link href={props.route}>
-                        <a className="btn btn-primary">Go</a>
+                        <a className={`btn btn-primary ${styles.shadow}`}>Go</a>
                     </Link>
                 </div>
             </div>
