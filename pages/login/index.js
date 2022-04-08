571a4b93dd190812bd7e3ff67ccf884889ab10fd
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-04-08 18:11:00 +0300

    Update index.js

--- pages/login/index.js
+++ pages/login/index.js
@@ -26,35 +26,18 @@ export default function LoginPage() {
 
     const loadMetamaskIcon = () => {
         const viewer = ModelViewer({
-            // Dictates whether width & height are px or multiplied
             pxNotRatio: true,
-            width: 50,
-            height: 50,
-            // pxNotRatio: false,
-            // width: 0.9,
-            // height: 0.9,
-            
-            // To make the face follow the mouse.
+            width: 75,
+            height: 75,
             followMouse: false,
-            
-            // head should slowly drift (overrides lookAt)
             slowDrift: false,
         });
         
-        // add viewer to DOM
         const container = document.getElementById('logo-container');
-        container && container.appendChild(viewer.container);
+        container.appendChild(viewer.container);
 
-        // look at something on the page
-        viewer.lookAt({
-            x: 100,
-            y: 100,
-        });
-
-        // enable mouse follow
+        viewer.lookAt({ x: 100, y: 100, });
         viewer.setFollowMouse(true);
-
-        // deallocate nicely
         viewer.stopAnimation();
     }
 
